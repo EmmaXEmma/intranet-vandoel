071512f065af1af27e4761d57853bbee24339867
EmmaXEmma <emanueltailer123@gmail.com>
2025-07-15 18:29:17 -0500

    fix: binding Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -260,7 +260,11 @@ def logout():
         icono=icono,
         audio=audio)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
